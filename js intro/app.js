65218eff73579e76943af15952a057c027301d0a
Ashish Shukla <akshkgd@gmail.com>
2022-11-29 22:24:46 +0530

    wd

---
+++ js intro/app.js
@@ -0,0 +1,31 @@
+// console.log('app.js is connected!')
+// variables => (var), es6=> let and const 
+
+// var email = "ashish@gmail.com";
+// let name = 'ashish';
+
+// const gst = 18;
+// // gst = 10;
+// console.log(gst)
+
+
+// data types 
+// strings => char or combinations of char => '' or "" 
+let name = 'ahsish';
+let nicknname = "kumar";
+// numbers => never use quotes with numbers
+let age = 25.6;
+// boolean => true or false
+let isAlive = true;
+
+
+// console.log(typeof(nicknname))
+// console.log(typeof name);
+console.log(typeof isAlive)
+
+
+// arrays and objects => (detail)
+
+
+
+
