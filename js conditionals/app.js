4c3484305a312ca9a3ac8cda9008a8b5dd33dbce
Ashish Shukla <akshkgd@gmail.com>
2022-12-08 08:53:16 +0530

    wd

---
+++ js conditionals/app.js
@@ -0,0 +1,65 @@
+// conditionals => if and else
+// let age = prompt('Enter the player age ')
+
+// if(age >= 18)
+//     console.log('eligible to vote!');
+// else
+//     console.log('not eligible to vote!');
+
+// tournament  => (16 24)
+// and or or gates
+
+// if(age >=16){
+//     if(age<=24){
+//         console.log('Eligible to play!')
+//     }
+//     else{
+//         console.log('Age is more than 24')
+
+//     }
+// }
+// else{
+//     console.log('Age is less than 16')
+// }
+// multiple conditions
+// let condition = 'dnksfksf'
+
+// if(condition == 'sunny')
+//     console.log('Wear normal cloths!');
+// else if(condition == 'raining')
+//     console.log('Wear rain coat');
+// else if(condition == 'cold')
+//     console.log('Wear jacket');
+// else{
+//     console.log('error')
+// }
+
+
+// ternary operater
+// age = 3;
+// age >= 18 ? console.log("eligible") ?  : console.log('not eligible')
+
+
+// console.log('ashish')
+
+
+// for loop
+let number = prompt('Enter a number')
+// for (let i=1; i<=100; i++ ){
+//     if (i%2 == 0){
+//         console.log(i, "is even" )
+//     }
+//     else{
+//         console.log(i, "is odd")
+
+//     }
+// }
+
+
+
+if (number%2 == 0){
+    console.log(number, "is even" )
+}
+else{
+    console.log(number, "is odd")
+}
