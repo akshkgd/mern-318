fa75b70d723991553ce2d6288ea6de1780f799fa
Ashish Shukla <akshkgd@gmail.com>
2023-01-19 20:24:54 +0530

    wd

--- movie js/app.js
+++ movie js/app.js
@@ -1,7 +1,20 @@
+let title = document.getElementById('title');
+let desc = document.getElementById('desc');
+let year = document.getElementById('year');
+let genre = document.getElementById('genre');
+let actors = document.getElementById('actors');
+let director = document.getElementById('director');
+let earnings = document.getElementById('earnings');
+let ratings = document.getElementById('ratings');
+let img = document.getElementById('img');
+let awards = document.getElementById('awards')
+let container = document.getElementById('container')
+let loader = document.getElementById('loader')
 let api = 'http://www.omdbapi.com/?apikey=61e576a4&t=';
 
 
 function searchMovie() {
+    loader.classList.add('loader')
     let userInput = document.getElementById('movieName').value;
     let searchKeyword = api + userInput;
     console.log(searchKeyword)
@@ -12,6 +25,16 @@ function searchMovie() {
         })
         .then(function (data) {
             console.log(data);
-            
+            title.innerText = data.Title;
+            desc.innerText = data.Plot;
+            actors.innerText = data.Actors;
+            director.innerText = data.Director;
+            ratings.innerText = data.Ratings[0].Value;
+            earnings.innerText = data.BoxOffice;
+            img.src = data.Poster;
+            awards.innerText = data.Awards;
+            year.innerText = data.Year;
+            container.classList.remove('d-none')
+            loader.classList.remove('loader')
         });
 }
--- todo js/app.js
+++ todo js/app.js
@@ -1,11 +1,88 @@
-// let today  = new Date();
-// const options = {day:'numeric',  month: 'long', }
+let today  = new Date();
+const options = { weekday: 'long', day:'numeric',  month: 'long', }
 // console.log(today.toLocaleDateString('en', options))
 
 let todos = []
+function checkNoTodo(){
+    if(todos.length == 0){
+        let noTodo = document.getElementById('no-todo');
+        noTodo.classList.remove('d-none')
+        noTodo.classList.add('no-todo')
+    }
+    else{
+        let noTodo = document.getElementById('no-todo');
+        noTodo.classList.remove('no-todo')
+        noTodo.classList.add('d-none')
+    }
+}
 
+function makeInputsBlank(){
+    document.getElementById('title').value = '';
+    document.getElementById('desc').value = '';
+    document.getElementById('dueDate').value = '';
+}
+
+function renderTodos(){
+    let todoContainer = document.getElementById('todoContainer');
+    todoContainer.innerHTML = ''
+    todos.map((todo)=>{
+        let todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('todo-done');
+
+        let contentDiv = document.createElement('div');
+        contentDiv.classList.add('content');
+
+        let title = document.createElement('p');
+        title.classList.add('todo-title');
+        let desc = document.createElement('p');
+        desc.classList.add('desc');
+        let dueDate = document.createElement('p');
+        dueDate.classList.add('due');
+
+        // adding values
+        title.innerText = todo.title;
+        desc.innerText = todo.desc;
+        let date = new Date(todo.dueDate);
+        let fDate = date.toLocaleDateString('en', options);
+        let fTime = date.toLocaleTimeString('en-US')
+        dueDate.innerText = fTime + " " + fDate;
+
+
+        todoContainer.appendChild(todoDiv)
+        todoDiv.appendChild(checkbox);
+        todoDiv.appendChild(contentDiv);
+        contentDiv.appendChild(title);
+        contentDiv.appendChild(desc);
+        contentDiv.appendChild(dueDate);
+
+    })
+}
+function sortTodos(){
+    let sorted = todos.sort((a,b)=>{
+        let dateA = new Date(a.dueDate);
+        let dateB = new Date(b.dueDate);
+
+        // compare
+        if(dateA < dateB){
+            return -1;
+        }
+        else if(dateA > dateB){
+            return 1;
+        }
+        else{
+            return 0
+        }
+
+        
+    })
+    console.log("sorted: ", sorted)
+}
 
 function addTodo(){
+    
     let title = document.getElementById('title').value;
     let desc = document.getElementById('desc').value;
     let dueDate = document.getElementById('dueDate').value;
@@ -17,4 +94,10 @@ function addTodo(){
 
     todos.push(tempTodoObject);
     console.log(todos)
+    checkNoTodo();
+    makeInputsBlank();
+    sortTodos();
+    renderTodos();
+
 }
+
