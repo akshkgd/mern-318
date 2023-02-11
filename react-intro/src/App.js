03b182a8644e24a75a418602b2132cdb60e5b478
Ashish Shukla <akshkgd@gmail.com>
2023-02-11 23:15:53 +0530

    wd

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import Test from './components/FirstComponent.js'
 import './App.css';
 let age = 26;
-const menu = [  {    name: "Pizza",    description: "Hot and cheesy with your choice of toppings.",    img: "pizza"  },  {    name: "Burger",    description: "Juicy beef patty, topped with cheese.",    img: "burger"  },  {    name: "Tacos",    description: "Spicy chicken or beef in a warm tortilla.",    img: "tacos"  },  {    name: "Sushi",    description: "Fresh fish and rice rolled in seaweed.",    img: "sushi"  },  {    name: "Ramen",    description: "Japanese noodle soup with savory broth.",    img: "ramen"  },  {    name: "Biryani",    description: "Indian spiced rice dish with meat or veggies.",    img: "biryani"  }];
+const menu = [  {    name: "Pizza",    description: "Hot and cheesy with your choice of toppings.",    img: "pizza", rating: 4.5, votes: 323, price: 299  },  {    name: "Burger",    description: "Juicy beef patty, topped with cheese.",    img: "burger",rating: 4.4 ,votes: 2000, price: 99  },  {    name: "Tacos",    description: "Spicy chicken or beef in a warm tortilla.",    img: "tacos", rating: 3.9, votes: 45 , price: 399 },  {    name: "Sushi",    description: "Fresh fish and rice rolled in seaweed.",    img: "sushi", rating: 4.3, votes:559, price: 299  },  {    name: "Ramen",    description: "Japanese noodle soup with savory broth.",    img: "ramen",rating: 4.0, votes: 98, price: 249  },  {    name: "Biryani",    description: "Indian spiced rice dish with meat or veggies.",    img: "biryani", rating: 4.9, votes: 903, price: 199 }];
 
 function App() {
   return (
     <div className="food-container">
       {
         menu.map((item)=>{
-          return <Test name={item.name} desc={item.description} img={item.img}/>
+          return <Test name={item.name} desc={item.description} img={item.img} rating={item.rating} votes={item.votes} price={item.price}/>
         })
       }
       
--- react-intro/src/components/FirstComponent.js
+++ react-intro/src/components/FirstComponent.js
@@ -1,10 +1,19 @@
 function FirstComponent(props){
+    function orderFoodHandler(){
+        console.log("Order successful for ", props.name, " at ", props.price, " rs.")
+    }
     return(
-        <div className="card">
-            <img className="card-img" src={"https://source.unsplash.com/1600x800/?" + props.img} />
-            <h3>{props.name}</h3>
+        <div className="card" onClick={orderFoodHandler}>
+            <div className="card-header">
+                <img className="card-img" src={"https://source.unsplash.com/1600x800/?" + props.img} />
+                <p className="ratings">{props.rating} ({props.votes} Votes)</p>
+            </div>
+            <div className="flex">
+                <h3>{props.name}</h3>
+                <p className={props.rating> 4.4 ? "tag" : props.rating >4 && props.votes> 1000? "popular" : "d-none"}>{props.rating> 4.4 ? "Must Try" : props.rating > 4 && props.votes> 1000? "Most Popular" : ""}</p>
+            </div>
             <p>{props.desc}</p>
-            <p>4.5</p>
+            {/* <p>4.5</p> */}
         </div>
         
     )
